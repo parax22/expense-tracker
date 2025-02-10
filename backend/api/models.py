a4d6b4ff293141dae9b4221916a7bcacff8dab47
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2025-02-10 17:17:44 -0500

    feat(backend): update expense model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,8 +6,8 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='PEN')
     date = models.DateField()
-    description = models.TextField(max_length=100, default="No description")
-    category = models.CharField(max_length=50)
+    description = models.TextField(max_length=50, default="No description")
+    category = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='expenses')
 
     def __str__(self):
